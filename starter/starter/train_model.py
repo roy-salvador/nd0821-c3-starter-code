fdf2a6b0e9a64a8b505d1b869208f9d5b8bf411a
roy-salvador <roy.salvador@gmail.com>
2022-01-16 14:14:39 +0800

    Clean starter code for flake8

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,14 @@
 # Script to train machine learning model.
 
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+# Optional enhancement, use K-fold cross validation
+# instead of a train-test split.
+# train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
     "workclass",
@@ -19,9 +20,9 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+# X_train, y_train, encoder, lb = process_data(
+#    train, categorical_features=cat_features, label="salary", training=True
+# )
 
 # Proces the test data with the process_data function.
 
